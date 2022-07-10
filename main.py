0fe7819d5aa9b8fefb1c64b801fd718a272d8357
Hansel López <halm29799@gmail.com>
2022-07-10 13:18:42 -0600

    Create Renderer and First Image

---
+++ main.py
@@ -0,0 +1,14 @@
+from gl import Renderer
+
+rend = Renderer(1024, 512)
+
+rend.glClearColor(0, 0.5, 0)
+rend.glColor(1, 1, 0)
+rend.glClear()
+
+# rend.glPoint(100, 100)
+
+for i in range(512):
+    rend.glPoint(i, i)
+
+rend.glFinish("output.bmp")
