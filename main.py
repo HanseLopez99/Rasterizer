b494969f05c3f91952637376d568539eb43dcdbd
Hansel Lopez <halm29799@gmail.com>
2023-07-07 13:38:27 -0600

    Draw point and generate bmp file

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ from gl import Renderer
 
 rend = Renderer(1024, 512)
 
-rend.glClearColor(0, 0.5, 0)
-rend.glColor(1, 1, 0)
-rend.glClear()
+# rend.glClearColor(0, 0.5, 0)
+# rend.glColor(1, 1, 0)
+# rend.glClear()
 
-# rend.glPoint(100, 100)
+rend.glPoint(100, 100, rend.glColor(1, 0, 0))
 
-for i in range(512):
-    rend.glPoint(i, i)
+# for i in range(512):
+#     rend.glPoint(i, i)
 
 rend.glFinish("output.bmp")
