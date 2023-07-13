3d19d4675d010189c14be1059f43b07a08738c67
Hansel Lopez <halm29799@gmail.com>
2023-07-13 17:53:29 -0600

    Add glLine and create some examples with  glPoint

--- gl.py
+++ gl.py
@@ -1,4 +1,7 @@
 import struct
+from collections import namedtuple
+
+V2 = namedtuple("Point2", ["x", "y"])
 
 
 def char(c):
@@ -26,6 +29,8 @@ class Renderer(object):
         self.height = height
         self.clearColor = color(0, 0, 0)
         self.currentColor = color(1, 1, 1)
+        # default color white
+        self.color = color(1, 1, 1)
 
         self.glClear()
 
@@ -43,6 +48,71 @@ class Renderer(object):
         self.pixels = [
             [self.clearColor for y in range(self.height)] for x in range(self.width)
         ]
+
+    def glLine(self, v0, v1, clr=None):
+        # Bresenham's line algorithm
+        # y = mx + b
+
+        # m = (v1.y - v0.y) / (v1.x - v0.x)
+        # y = v0.y
+
+        # for x in range(v0.x, v1.x + 1):
+        #     self.glPoint(x, int(y), clr)
+
+        x0 = int(v0.x)
+        x1 = int(v1.x)
+        y0 = int(v0.y)
+        y1 = int(v1.y)
+
+        # Si el punto 0 es igual al punto 1, solo dibujar un punto
+        if x0 == x1 and y0 == y1:
+            self.glPoint(x0, y0)
+            return
+
+        dy = abs(y1 - y0)
+        dx = abs(x1 - x0)
+
+        steep = dy > dx
+
+        # Si la linea tiene pendiente mayor a 1 o menor a -1
+        # intercambiar el eje x por el y
+        # Dibujar de manera vertical en vez de horizontal
+        if steep:
+            x0, y0 = y0, x0
+            x1, y1 = y1, x1
+
+        # Si el punto inicial en x es mayor al punto final en x,
+        # intercambiar los puntos para siempre dibujar de izquierda a derecha
+        if x0 > x1:
+            x0, x1 = x1, x0
+            y0, y1 = y1, y0
+
+        dy = abs(y1 - y0)
+        dx = abs(x1 - x0)
+
+        offset = 0
+        limit = 0.5
+
+        m = dy / dx
+        y = y0
+
+        for x in range(x0, x1 + 1):
+            if steep:
+                # Dibujar de manera vertical
+                self.glPoint(y, x, clr or self.currentColor)
+            else:
+                # Dibujar de manera horizontal
+                self.glPoint(x, y, clr or self.currentColor)
+
+            offset += m
+
+            if offset >= limit:
+                if y0 < y1:
+                    y += 1
+                else:
+                    y -= 1
+
+                limit += 1
 
     def glFinish(self, fileName):
         with open(fileName, "wb") as file:
--- main.py
+++ main.py
@@ -1,14 +1,47 @@
-from gl import Renderer
+from gl import Renderer, V2, color
+import random
 
-rend = Renderer(1024, 512)
+width = 1920
+height = 1080
+
+rend = Renderer(width, height)
 
 # rend.glClearColor(0, 0.5, 0)
 # rend.glColor(1, 1, 0)
 # rend.glClear()
 
-rend.glPoint(100, 100, rend.glColor(1, 0, 0))
+# rend.glPoint(100, 100, rend.glColor(1, 0, 0))
 
-# for i in range(512):
-#     rend.glPoint(i, i)
+# for x in range(0, width, 10):
+#     for y in range(0, height, 10):
+#         rend.glLine(V2(0, 0), V2(x, height - 1))
+#         rend.glLine(V2(0, 0), V2(width - 1, y))
+
+# for x in range(width):
+#     for y in range(height):
+#         pixelColor = rend.glColor(random.random(), random.random(), random.random())
+#         rend.glPoint(x, y, pixelColor)
+
+# Stars
+for x in range(width):
+    for y in range(height):
+        if random.random() > 0.99:
+            size = random.randrange(1, 3)
+            brighness = random.random() / 2 + 0.5
+            starColor = color(brighness, brighness, brighness)
+            if size == 0:
+                rend.glPoint(x, y, starColor)
+            elif size == 1:
+                rend.glPoint(x, y, starColor)
+                rend.glPoint(x + 1, y, starColor)
+                rend.glPoint(x + 1, y + 1, starColor)
+                rend.glPoint(x, y + 1, starColor)
+            elif size == 2:
+                rend.glPoint(x, y, starColor)
+                rend.glPoint(x, y + 1, starColor)
+                rend.glPoint(x, y - 1, starColor)
+                rend.glPoint(x + 1, y, starColor)
+                rend.glPoint(x - 1, y, starColor)
+
 
 rend.glFinish("output.bmp")
