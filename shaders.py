29ef0e8590b62937882a54876103612ba5915b27
Hansel Lopez <halm29799@gmail.com>
2023-08-02 23:54:59 -0600

    Revert "Replace numpy with my math library"
    This reverts commit 1720713a380e5c37a760ee4ea60332a9a763da58.

--- shaders.py
+++ shaders.py
@@ -1,15 +1,13 @@
-import mathLib as ml
-
-
 def vertexShader(vertex, **kwargs):
     modelMatrix = kwargs["modelMatrix"]
 
     vt = [vertex[0], vertex[1], vertex[2], 1]
 
-    # Operate @ for: 'list' and 'list'
-    vt = ml.matmul4(modelMatrix, vt)
+    vt = modelMatrix @ vt
 
-    vt = [vt[0], vt[1], vt[2]]
+    vt = [vt[0, 0], vt[0, 1], vt[0, 2], vt[0, 3]]
+
+    vt = [vt[0] / vt[3], vt[1] / vt[3], vt[2] / vt[3]]
 
     return vt
 
